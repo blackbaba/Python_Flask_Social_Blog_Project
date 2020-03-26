dcf2c29f7e39a9a9eb5c748c1d93eb90b8fb5571
Amrinder Dhindsa <amrinder.dhindsa1@gmail.com>
2020-03-26 19:31:04 +0800

    Setup up app

--- hello.py
+++ hello.py
@@ -4,4 +4,17 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return '<h1>Hello World !</h1>'
+    return '<h1>Hello World!</h1>'
+
+
+@app.route('/user/<name>')
+def user(name):
+    return '<h1>Hello, {}!</h1>'.format(name)
+
+
+# app.add_url_rule('/', 'index', index)
+# SET FLASK_APP=hello.py
+# SET FLASK_DEBUG=1
+
+# if __name__ == '__main__':
+#     app.run()
