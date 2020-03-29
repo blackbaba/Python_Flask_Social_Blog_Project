0f6e62ac35707f6cbf398b3b0e04f50ecebf3c9d
Amrinder Dhindsa <amrinder.dhindsa1@gmail.com>
2020-03-29 19:38:56 +0800

    Create flash notices

--- hello.py
+++ hello.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime
+
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
@@ -11,10 +12,17 @@ app.config['SECRET_KEY'] = 'hard to guess string aha..'
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
-# Root route
-@app.route('/')
+# Form POST route
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html', current_time=datetime.utcnow())
+    form = NameForm()
+    if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('Looks like you have changed your name!')
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form, name=session.get('name'))
 
 
 # User route
