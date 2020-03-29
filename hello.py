fc2b081b236198663bf7cc2d92d8138236f2298d
Amrinder Dhindsa <amrinder.dhindsa1@gmail.com>
2020-03-29 16:46:13 +0800

    Create templates

--- hello.py
+++ hello.py
@@ -1,20 +1,32 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 
-
+# Root route
 @app.route('/')
 def index():
-    return '<h1>Hello World!</h1>'
+    return render_template('index.html')
 
-
+# User route
 @app.route('/user/<name>')
 def user(name):
-    return '<h1>Hello, {}!</h1>'.format(name)
+    return render_template('user.html', name=name)
+
+# Error routes
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
 
 
-# app.add_url_rule('/', 'index', index)
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
+
+# app.add_url_route('/', 'index', index)
 # SET FLASK_APP=hello.py
 # SET FLASK_DEBUG=1
 
 # if __name__ == '__main__':
-#     app.run()
+    # app.run(debug=True)
