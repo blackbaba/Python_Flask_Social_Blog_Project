7f90d838b2d7974d5353f5a8b5aa6b3d6ba2cffc
Amrinder Dhindsa <amrinder.dhindsa1@gmail.com>
2020-06-29 21:58:27 +0800

    Setup Jinja templates

--- hello.py
+++ hello.py
@@ -1,7 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template, redirect
+from flask_bootstrap import Bootstrap
+
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 
 # index route
 @app.route('/')
 def index():
-    return '<h1>Hello World from Flask </h1>'
+    return render_template('index.html')
+
+# user route
+@app.route('/user/<name>')
+def user(name):
+    return render_template('user.html', name=name)
